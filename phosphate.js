5010237828a670c6d692256234c09f138daa9ed7
hiccup01 <yectoplasma@icloud.com>
2015-10-31 14:30:53 +1300

    Made it check for jQuery

--- phosphate.js
+++ phosphate.js
@@ -1,9 +1,17 @@
-function () { 
-    var jsCode = document.createElement('script'); 
-    jsCode.setAttribute('src', 'http://code.jquery.com/jquery-1.11.3.min.js');                  
-  document.body.appendChild(jsCode); 
- }();
- setTimeout(function() {
+if (!window.jQuery) {
+  var jq = document.createElement('script'); jq.type = 'text/javascript';
+  // Path to jquery.js file, eg. Google hosted version
+  jq.src = '/path-to-your/jquery.min.js';
+  document.getElementsByTagName('head')[0].appendChild(jq);
+  $(document).ready(function(){
+	alert('Code ran.');
+    var scratch_player = $(".stage");
+    var url = window.location.href;
+    url = url.substr(33,40);
+    $(scratch_player).empty();
+    $(scratch_player).append("<script src=https://phosphorus.github.io/embed.js?id=", url, "&auto-start=false&light-content=false></script>");
+});
+} else {
 $(document).ready(function(){
 	alert('Code ran.');
     var scratch_player = $(".stage");
@@ -12,6 +20,6 @@ $(document).ready(function(){
     $(scratch_player).empty();
     $(scratch_player).append("<script src=https://phosphorus.github.io/embed.js?id=", url, "&auto-start=false&light-content=false></script>");
 });
- }, 500);
-};
+}
+
 
