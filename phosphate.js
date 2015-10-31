d4f87d35928dffec769c5af9588b715e62d62812
hiccup01 <yectoplasma@icloud.com>
2015-10-31 14:14:17 +1300

    added timeout for the script so jQuery can hopefully work

--- phosphate.js
+++ phosphate.js
@@ -1,13 +1,16 @@
+function () { 
+    var jsCode = document.createElement('script'); 
+    jsCode.setAttribute('src', 'http://code.jquery.com/jquery-1.11.3.min.js');                  
+  document.body.appendChild(jsCode); 
+ }();
+ setTimeout(function() {
 $(document).ready(function(){
-	function () { 
-    var jQuery = document.createElement('script'); 
-    jQuery.setAttribute('src', 'http://code.jquery.com/jquery-1.11.3.min.js');                  
-  document.head.appendChild(jQuery); 
- }());
     var scratch_player = $(".stage");
     var url = window.location.href;
     url = url.substr(33,40);
     $(scratch_player).empty();
     $(scratch_player).append("<script src=https://phosphorus.github.io/embed.js?id=", url, "&auto-start=false&light-content=false></script>");
 });
+ }, 500);
+};
 
