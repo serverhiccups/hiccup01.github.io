69d75e1aed3ccea82e06789e8623627a5d96d13e
hiccup01 <hiccup01@users.noreply.github.com>
2015-12-06 16:28:37 +1300

    Create swiki.js
    A simple js program to search the scratch wiki

---
+++ swiki.js
@@ -0,0 +1,3 @@
+var searchterm = prompt("Enter your search term"); 
+var url = "http://wiki.scratch.mit.edu/w/index.php?title=Special%3ASearch&profile=default&search=" + searchterm + "&fulltext=Search"; 
+window.open(url);
