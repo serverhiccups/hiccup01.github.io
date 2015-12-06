612409e516df2c11645ddf8b893fe430efabb6ac
hiccup01 <hiccup01@users.noreply.github.com>
2015-12-06 17:37:05 +1300

    Update swiki.js

--- swiki.js
+++ swiki.js
@@ -1,3 +1,7 @@
 var searchterm = prompt("Enter your search term"); 
+if searchterm = null {
+  console.log("Error: null is invalid input.")
+} else {
 var url = "http://wiki.scratch.mit.edu/w/index.php?title=Special%3ASearch&profile=default&search=" + searchterm + "&fulltext=Search"; 
 window.open(url);
+}
