38e025908d5820ec5b16bea2a68345b8a1eafce5
hiccup01 <hiccup01@users.noreply.github.com>
2015-12-10 07:50:52 +1300

    Fixed a comparison

--- swiki.js
+++ swiki.js
@@ -1,5 +1,5 @@
 var searchterm = prompt("Enter your search term"); 
-if searchterm = null {
+if searchterm == null {
   console.log("Error: null is invalid input.")
 } else {
 var url = "http://wiki.scratch.mit.edu/w/index.php?title=Special%3ASearch&profile=default&search=" + searchterm + "&fulltext=Search"; 
