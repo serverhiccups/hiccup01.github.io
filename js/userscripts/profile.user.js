ecf37f4ccd7d12083d41fd742ce7cfcfc1608635
hiccup01 <yectoplasma@icloud.com>
2016-02-20 11:22:59 +1300

    Update ProfileWizard

--- js/userscripts/profile.user.js
+++ js/userscripts/profile.user.js
@@ -3,23 +3,31 @@
 // @namespace   https://scratch.mit.edu/users/hiccup01
 // @description Makes scratch profiles better
 // @include     https://scratch.mit.edu/users/*
-// @version     0.8
+// @version     1.0
 // @grant       none
 // @updateURL http://www.hiccup01.com/js/userscripts/profile.user.js
 // ==/UserScript==
-console.log("Running ProfileWizard v0.8");
+console.log("Running ProfileWizard v1.0");
+document.body.style.height = "auto";
 var aboutme = document.getElementsByClassName(".about");
 var status = document.querySelector("textarea[name=status]");
 var textarea = status.innerHTML;
-var colour = textarea.substr((textarea.indexOf("§") + 1), (textarea.indexOf("§") + 6));
-var url = textarea.substr((textarea.indexOf("≤")), (textarea.indexOf("≥")));
+var colour = textarea.substring((textarea.indexOf("§") + 1), (textarea.indexOf("§") + 8));
+var url = textarea.substring((textarea.indexOf("≤") + 1), (textarea.indexOf("≥")));
+console.log(url);
 colour.toString();
 url = encodeURI(url);
 var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colour);
-if (isOk  == true) {
-console.log("Set page colour to: " + colour);
-colour = colour.concat(" url(\"", url, "\") repeat"); 
+if (isOk  === true || url != "" || null || "null" || undefined) {
+if (isOk === true) {
+	console.log("Set page colour to: " + colour);
+	if (url != "" || null || "null" || undefined) {
+		colour = colour.concat(" url(\"", url, "\") repeat");
+	};
+} else {
+	colour = colour.concat(" url(\"", url, "\") repeat");
+}
 var bler = document.body.style.background = colour;
 } else {
- console.log("No colour found or invalid colour.");   
+console.log("No colour found or invalid colour.");
 }
