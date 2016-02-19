cc2f8de814bbcc7e22a17023bc34c40ecd7ccec1
hiccup01 <yectoplasma@icloud.com>
2016-02-20 09:59:30 +1300

    Hopefully add image support

--- js/userscripts/profile.user.js
+++ js/userscripts/profile.user.js
@@ -3,20 +3,23 @@
 // @namespace   https://scratch.mit.edu/users/hiccup01
 // @description Makes scratch profiles better
 // @include     https://scratch.mit.edu/users/*
-// @version     0.3
+// @version     0.7
 // @grant       none
 // ==/UserScript==
-console.log("Running ProfileWizard v0.3");
+console.log("Running ProfileWizard v0.7");
 var aboutme = document.getElementsByClassName(".about");
-var zstatus = document.querySelector("textarea[name=status]");
-var textarea = zstatus.innerHTML;
+var status = document.querySelector("textarea[name=status]");
+var textarea = status.innerHTML;
 var colour = textarea.substr((textarea.indexOf("§") + 1), (textarea.indexOf("§") + 6));
+var url = textarea.substr((textarea.indexOf("≤")), (textarea.indexOf("≥")));
 colour.toString();
+url = encodeURI(url);
 var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colour);
 if (isOk  == true) {
 console.log("Set page colour to: " + colour);
-colour = colour.concat(" none"); 
+colour = colour.concat(" url(\"", url, "\") repeat"); 
 var bler = document.body.style.background = colour;
 } else {
  console.log("No colour found or invalid colour.");   
 }
+≤≥
