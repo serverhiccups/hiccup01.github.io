801db6d65eb99d741a0dd80662e6b508bb81b643
hiccup01 <hiccup01@users.noreply.github.com>
2016-03-08 07:42:39 +1300

    Update profile.user.js

--- js/userscripts/profile.user.js
+++ js/userscripts/profile.user.js
@@ -6,9 +6,9 @@
 // @include     https://scratch.mit.edu/*
 // @exclude     https://scratch.mit.edu
 // @exclude     https://scratch.mit.edu/discuss/*
-// @version     2.8
+// @version     2.9
 // @grant       none
-// @updateURL   http://www.hiccup01.com/js/userscripts/profile.user.js
+// @updateURL   http://www.hiccup01.com/ProfileWizard/profile.user.js
 // @icon        http://www.hiccup01.com/img/pw.png
 // ==/UserScript==
 console.log("Running ProfileWizard v2.8");
