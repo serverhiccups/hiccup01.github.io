6a47773e38b901d748ffa89454f6a87c52e075cd
hiccup01 <yectoplasma@icloud.com>
2016-03-11 18:26:15 +1300

    add my first extension

---
+++ js/scratchextensions/sign.js
@@ -0,0 +1,41 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.sign = function(x) {
+        switch(Math.sign(x));
+            case 1:
+            return "+";
+            break;
+            case -1:
+            return "-";
+            break;
+            case 0:
+            return 0;
+            break;
+            case -0:
+            return 0;
+            break;
+            case NaN:
+            return NaN;
+            break;
+            default:
+            return "No value";
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+        ['r', "%n's sign", "sign", -3],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('sign', descriptor, ext);
+})({});
